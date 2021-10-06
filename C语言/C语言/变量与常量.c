7ee4483f35c9d5432901949eb77afed9217fd8cc
WithAndrew <1032286603@qq.com>
2021-10-06 11:43:49 +0800

    修改

--- C语言/C语言/变量与常量.c
+++ C语言/C语言/变量与常量.c
@@ -1,34 +1,32 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//
-//int main(void)
-//{
-////	int a = 12;
-////	a = 23;
-////	a = -1;
-//
-//	//13;
-//
-//	//a = 13;  // 左值---变量  右值 --- 常量
-//
-////	int a;
-////	int b;
-//	int a,  //人命
-//		b,  //年龄
-//		c,
-//		d;
-//
-//	a = -1;
-//	a = 2;
-//
-//	//printf ("%d\n", a);
-//
-//	b = sizeof(a);
-//
-//	printf ("%d\n", b);
-//
-//
-//	system("pause");
-//	return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //	int a = 12;
+    //	a = 23;
+    //	a = -1;
+
+    //13;
+
+    //a = 13;  // 左值---变量  右值 --- 常量
+
+    //	int a;
+    //	int b;
+    int a, //人命
+        b, //年龄
+        c,
+        d;
+
+    a = -1;
+    a = 2;
+
+    //printf ("%d\n", a);
+
+    b = sizeof(a);
+
+    printf("%d\n", b);
+
+    system("pause");
+    return 0;
+}
